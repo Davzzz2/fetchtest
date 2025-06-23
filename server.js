909c718496a136c73bdc225dd8a8706ac8ac72f0
Davzzz2 <123270465+Davzzz2@users.noreply.github.com>
2025-06-23 15:20:56 +0200

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-const express = require("express");
-const fetch = require("node-fetch");
-const mongoose = require("mongoose");
-const cron = require("node-cron");
+import express from "express";
+import mongoose from "mongoose";
+import fetch from "node-fetch";
+import WebSocket from "ws";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
